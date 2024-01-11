d102569d36a525f38955aa72b55ac1f6628075d0
IkezawaYuki <y.ikezawa93@gmail.com>
2024-01-12 00:27:53 +0900

    mod

--- function_app.py
+++ function_app.py
@@ -1,30 +1,42 @@
 import azure.functions as func
 import logging
 import os
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+import json
+import requests
+
 
 
 azure_app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+    
 
-@azure_app.route(route="http_trigger", auth_level=func.AuthLevel.FUNCTION)
+
+@azure_app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    try:
+        req_body = req.get_json()
+        slack_event = req_body.get('event')
+        if slack_event.get("type") == "app_mention":
+            user_id = slack_event.get("user")
+            channel_id = slack_event.get("channel")
+            send_slack_message(channel_id, f"<@{user_id}>さん、ファイヤー!!!!")
+    except ValueError as e:
+        print(e)
+    return func.HttpResponse("ファイヤー!", status_code=200)
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"ハロー, {name}.")
-    else:
-        return func.HttpResponse(
-             "うまくいってるぜ！",
-             status_code=200
-        )
+def send_slack_message(channel_id, message):
+    url = "https://slack.com/api/chat.postMessage"
+    token = os.environ.get("SLACK_BOT_TOKEN")
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "channel": channel_id,
+        "text": message
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code != 200:
+        logging.error(f"Slack APIへのリクエストに失敗しました。ステータスコード: {response.status_code}")
+
